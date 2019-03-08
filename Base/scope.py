6a104edce9c34b5916eb37ff0081af981677c6ee
adelliu <lqj679ssn@qq.com>
2019-03-08 13:34:52 +0800

    回复assert

--- Base/scope.py
+++ Base/scope.py
@@ -7,8 +7,8 @@ class ScopeInstance:
     send_email = Scope.get_scope_by_name('sendEmail', default=None).body
     send_mobile = Scope.get_scope_by_name('sendMobile', default=None).body
     read_app_list = Scope.get_scope_by_name('readMyAppList', default=None).body
-    # assert read_base_info
-    # assert write_base_info
-    # assert send_email
-    # assert send_mobile
-    # assert read_app_list
+    assert read_base_info
+    assert write_base_info
+    assert send_email
+    assert send_mobile
+    assert read_app_list
