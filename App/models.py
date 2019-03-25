77101a190f1053872f66ff9124fa12baeb578d1b
adelliu <lqj679ssn@qq.com>
2019-03-25 12:05:30 +0800

    debug

--- App/models.py
+++ App/models.py
@@ -497,6 +497,8 @@ class UserApp(models.Model):
     @classmethod
     def get_user_app_list_by_o_user(cls, o_user, frequent, count):
         app_list = cls.objects.filter(user=o_user, bind=True)
+        print(len(app_list))
+        print(o_user.to_dict())
         if frequent:
             if count < 0:
                 count = 3
