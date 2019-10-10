b92e2962a3d31f19cf25e5d4cd6303b1b276ebea
lqj679ssn <lqj679ssn@qq.com>
2019-10-10 13:41:42 +0800

    debug

--- App/api_urls.py
+++ App/api_urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from App.api_views import AppView, AppIDView, ScopeView, AppLogoView, UserAppIdView, \
-    AppIDSecretView, refresh_frequent_score, PremiseView, shorten_app_id
+    AppIDSecretView, refresh_frequent_score, PremiseView
 
 urlpatterns = [
     path('', AppView.as_view()),
@@ -9,7 +9,6 @@ urlpatterns = [
     path('premise', PremiseView.as_view()),
     path('logo', AppLogoView.as_view()),
     path('@refresh-frequent-score', refresh_frequent_score),
-    path('@shorten-app-id', shorten_app_id),
 
     path('user/<str:user_app_id>', UserAppIdView.as_view()),
     path('<str:app_id>', AppIDView.as_view()),
